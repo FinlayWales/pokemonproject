a158ba96f51e05f2c8edbdee2e88688ee579e704
Finlay Wales <finlaywales@gmail.com>
2021-06-01 20:41:38 +0100

    initial file upload

---
+++ imports/functions.py
@@ -0,0 +1,7 @@
+def flatten(foo):
+    for x in foo:
+        if hasattr(x, '__iter__') and not isinstance(x, str):
+            for y in flatten(x):
+                yield y
+        else:
+            yield x
---
+++ imports/secondaryeffects.py
@@ -0,0 +1,8 @@
+class secondaryeffects():
+    
+    def __init__(move):
+        super.__init__()
+        self.move = move
+
+    #Add functions for each status move/move with secondary effect
+        
